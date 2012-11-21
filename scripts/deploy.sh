9bc86e330177871a1457f5136568289a99318a32
Stefan Tilkov <stefan.tilkov@innoq.com>
2012-11-21 19:40:51 +0100

    changed deployment approach after Leiningen 2 upgrade

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/env ruby
 puts "Compiling & creating main jar."
-if system('lein jar')
-  mainjar = Dir['statuses-*.jar'].last
+if system('lein uberjar')
+  mainjar = Dir['target/statuses-*-standalone.jar'].last
+  mainjar['target/'] = '' # and I actually considered this language beautiful once?
   puts "Creating run script for #{mainjar}."
   f = File.new('./run.sh', 'w+')
-  f.puts "java -classpath #{mainjar}:#{Dir['./lib/*'].join(':')} statuses.server"
+  f.puts "java -jar #{mainjar}"
   f.close
   puts "Synchronizing with remote dir."
-  `rsync --exclude data --delete -avz run.sh #{mainjar} resources lib internal2.innoq.com:/home/statuses`
+  `rsync --exclude data --delete -avz run.sh target/#{mainjar} resources internal2.innoq.com:/home/statuses`
   puts "Done."
 end
 
