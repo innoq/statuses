5e4e4680349f7119aef7a984d640c784e4ca03d6
Stefan Tilkov <stefan.tilkov@innoq.com>
2012-12-11 23:26:45 +0100

    Adapted configuration, first step for innoQ independence

--- config.clj
+++ config.clj
@@ -4,5 +4,5 @@
  :http-port     8080
  :run-mode      :dev
  ; {username} is replaced with the username
- ;:avatar-url   "https://testldap.innoq.com/liqid2/users/{username}/avatar/32x32"}
+ ;:avatar-url   "https://.../users/{username}/avatar/32x32"}
  :avatar-url    "http://assets.github.com/images/gravatars/gravatar-user-420.png"}
