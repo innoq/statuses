d01bf116902cde623d14433043a6674d8eba7963
Stefan Tilkov <stefan.tilkov@innoq.com>
2013-02-28 08:30:57 +0100

    updated for new server infrastructure

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,10 +5,11 @@ if system('lein uberjar')
   mainjar['target/'] = '' # and I actually considered this language beautiful once?
   puts "Creating run script for #{mainjar}."
   f = File.new('./run.sh', 'w+')
+  f.chmod(0744)
   f.puts "java -jar #{mainjar} $*"
   f.close
   puts "Synchronizing with remote dir."
-  `rsync --exclude data --delete -avz run.sh target/#{mainjar} public internal2.innoq.com:/home/statuses`
+  `rsync --exclude data --delete -avz run.sh target/#{mainjar} public statuses@internal2.innoq.com:/home/statuses`
   puts "Done."
 end
 
