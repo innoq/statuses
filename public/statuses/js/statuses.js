dfc30de7edbd7d8648313af4dcfc0cfe987476cf
Alexander Heusingfeld <aheusingfeld@firstpoint.de>
2014-10-23 01:11:06 +0200

    Add image preview #70

--- public/statuses/js/statuses.js
+++ public/statuses/js/statuses.js
@@ -5,27 +5,53 @@
 
 "use strict";
 
-
 $("#text").charCount(140);
 
+function focusField(fieldName) {
+    // set cursor to the end of the inserted content
+    fieldName.focus(function() {
+        var val = this.value;
+        this.value = '';
+        this.value = val;
+    });
+    fieldName.focus();
+}
 // quick reply
 $(".updates").on("click", ".post-content", function(ev) {
     var post = $(this).closest(".post");
     if(post.find(".new-reply").length) {
-        $(".new-reply input[name=text]", post).focus();
+        focusField($(".new-reply input[name=text]", post));
     } else {
         var postURI = $("a.permalink", post).attr("href");
         $('<div class="new-reply" />').appendTo(post)
             .load(postURI + " .update + form", function( response, status, xhr ) {
                     var input = $(".new-reply input[name=text]", post);
                     input.charCount(140);
-                    input.focus(function() {
-                        var val = this.value;
-                        this.value = '';
-                        this.value = val;
-                    });
-                    input.focus();
+                    focusField(input);
             }); // XXX: bad selector? -- XXX: introduces duplicate IDs
     }
+});
+
+var imgRegEx = /!(http[^\s]+)/gi;
+var markdownImgRegEx = /!\[(.+)\]\((http[^\s]+)\)/gi;
+$('.post-content').each(function() {
+    var contentText = $(this);
+    var result = contentText.text().replace(markdownImgRegEx, function(match, p1, p2, offset, string) {
+        $('<img src="' + p2 + '" alt="' + p1 + '" />').insertAfter(contentText);
+        if (p1 != null) {
+            return "";
+        }
+        return match;
+    });
+    if (result != contentText.text()) contentText.text(result);
+
+    result = contentText.text().replace(imgRegEx, function(match, p1, offset, string) {
+        $('<img src="' + p1 + '" alt="image" />').insertAfter(contentText);
+        if (p1 != null) {
+            return "";
+        }
+        return match;
+    });
+    if (result != contentText.text()) contentText.text(result);
 });
 }(jQuery));
