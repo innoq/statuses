1f66fd5be90756d85cfbe93a3301ce593a8070a7
Alexander Heusingfeld <aheusingfeld@firstpoint.de>
2014-10-29 00:10:46 +0100

    proposal to fix #74: add reply button
    - added "reply" button to the left of the "delete" button
    - removed the click listener from the .post-content

--- public/statuses/js/statuses.js
+++ public/statuses/js/statuses.js
@@ -12,7 +12,7 @@ var entryFormButton = $(".entry-form button");
 $("#entry-text").charCount(140, entryFormButton);
 
 // quick reply
-$(".updates").on("click", ".post-content", function(ev) {
+$(".btn-reply").on("click", function(ev) {
     $("div.new-reply").remove();
     var post = $(this).closest(".post");
     var postURI = $("a.permalink", post).attr("href");
