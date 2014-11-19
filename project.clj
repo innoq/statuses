6f6c05708060b8b73aa6942d38fb263725d15bfe
Michael Vitz <michael.vitz@innoq.com>
2014-11-19 13:19:06 +0100

    Clean up project.clj.
    Relates to #113.
    
    - remove undocumented :namespaces
    - simple alias doesn't require vector
    - order changed to the one used in the official documentation
    - add url and license metadata
    - build aborts on conflicting dependency versions
    - uberjar profile fixes warning that main will be no longer included in
      aot by default with lein 3.x

--- project.clj
+++ project.clj
@@ -1,16 +1,21 @@
 (defproject statuses "0.1.0-SNAPSHOT"
   :description "Statuses app for innoQ"
-  :namespaces [statuses]
-  :dependencies [
-                 [org.clojure/clojure "1.6.0"]
+  :url "https://github.com/innoq/statuses"
+  :license {:name "Apache License, Version 2.0"
+            :url "http://www.apache.org/licenses/LICENSE-2.0"
+            :distribution :repo
+            :comments "A business-friendly OSS license"}
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [ring "1.3.1"]
                  [compojure "1.2.1"]
                  [clj-time "0.8.0"]
-                 [ring "1.3.1"]
                  [org.clojure/data.json "0.2.5"]]
+  :pedantic? :abort
   :plugins [[jonase/eastwood "0.1.5"]
             [lein-ring "0.8.13"]]
-  :ring {:handler statuses.views.main/app-routes}
+  :profiles {:dev {:dependencies [[ring-mock "0.1.5"]]}
+             :uberjar {:aot [statuses.server]}}
   :main statuses.server
-  :aliases {"lint"  ["eastwood"]}
-  :profiles {:dev {:dependencies [[ring-mock "0.1.5"]]}})
+  :aliases {"lint" "eastwood"}
+  :ring {:handler statuses.views.main/app-routes})
 
