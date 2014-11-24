aff5527c9dae32c0f80badb389dc192133702120
Alexander Heusingfeld <aheusingfeld@firstpoint.de>
2014-11-24 12:31:02 +0100

    Cleanup
    - use font awesome icons in navbar as requested by @mvitz
    - introduce a `button` function to capsulate boiler plate code

--- src/statuses/views/main.clj
+++ src/statuses/views/main.clj
@@ -27,7 +27,12 @@
     (if format (str "&format=" (name format)) ""))))
 
 (defn- glyphicon [icon]
-  [:span {:class (str "glyphicon glyphicon-" icon)}])
+  [:span {:class (str "fa fa-" icon)}])
+
+(defn- button
+  ([class label] (button class label nil))
+  ([class label icon]
+  [:button {:type "submit" :class (str "btn btn-" class)} (html (glyphicon icon) [:span.btn-label label])]))
 
 (defn- nav-link [url title icon]
   [:li (link-to url (glyphicon icon) title)])
@@ -41,35 +46,34 @@
 (defn nav-links [request]
   (let [github-issue-uri "https://github.com/innoq/statuses/issues"
         info-uri         "/statuses/info"]
-    (list (nav-link (mention-uri request)       "Mentions"        "user")
+    (list (nav-link (mention-uri request)       "Mentions"        "at")
           ; too many navbar items break the navbar layout at ~850px screen width
           ;(nav-link (updates-uri request :atom) "Feed (all)"      "fire")
-          (nav-link (mention-uri request :atom) "Feed (mentions)" "fire")
-          (nav-link info-uri                    "Info"            "info-sign")
-          (nav-link github-issue-uri            "Issues"           "question-sign")
-          (preference "inline-images"           "Inline images?"  "wrench"))))
+          (nav-link (mention-uri request :atom) "Feed (mentions)" "rss")
+          (nav-link info-uri                    "Info"            "info")
+          (nav-link github-issue-uri            "Issues"           "github")
+          (preference "inline-images"           "Inline images?"  "cogs"))))
 
 (defn format-time [time]
   [:time {:datetime (time/time-to-utc time)} (time/time-to-human time)])
 
 (defn delete-form [id]
   (form-to {:class "delete-form" :onsubmit "return confirm('Delete status?')"} [:delete (str base "/" id)]
-    (html [:button {:type "submit" :class "btn btn-delete"} (html [:span.fa.fa-remove ][:span.btn-label "Delete"])])
-    ))
+    (button "delete" "Delete" "remove")))
 
 (defn entry-form []
   (form-to {:class "entry-form"} [:post base]
     [:div.input.input-group
      (text-field {:class "form-control" :autofocus "autofocus"} "entry-text")
      [:span.input-group-btn
-      [:button {:type "submit" :class "btn btn-default"} "Send" ]]]
+      (button "default" "Send")]]
     [:div {"style" "clear: both"}]))
 
 (defn reply-form [id author request]
   (common/simple
     (form-to {:class (str "reply-form form" id)} [:post base]
       [:div.input-group (text-field {:class "form-control" :autofocus "autofocus" :value (str "@" author " ")} "reply-text")
-       [:span.input-group-btn [:button {:type "submit" :class "btn btn-default"} "Reply"]]]
+       [:span.input-group-btn (button "default" "Reply")]]
       (hidden-field "reply-to" id)
       [:div {"style" "clear: both"}])))
 
@@ -81,26 +85,15 @@
     [:div.meta
      [:span.author (link-to (str base "?author=" author) author)]
      (if in-reply-to
-       (list
-         [:span.reply (link-to (str base "/" in-reply-to) in-reply-to)]))
-     [:span.actions
-      [:button {:type "submit" :class "btn btn-reply"} (html [:span.fa.fa-reply ][:span.btn-label "Reply"])]
+       [:span.reply (link-to (str base "/" in-reply-to) in-reply-to)])
+     [:span.actions (button "reply" "Reply" "reply")
       (if can-delete?
-        (list
-          [:span.delete (delete-form id)]))]
+        [:span.delete (delete-form id)])]
      [:span.time [:a.permalink {:href (str base "/" id)} (format-time time)]]
      ]
     [:div.post-content (common/linkify text)]
   )
 )
-
-(defn entry-form []
-  (form-to {:class "entry-form"} [:post base]
-         [:div.input.input-group
-           (text-field {:class "form-control" :autofocus "autofocus"} "entry-text")
-           [:span.input-group-btn
-             [:button {:type "submit" :class "btn btn-default"} "Send" ]]]
-         [:div {"style" "clear: both"}]))
 
 (defn list-page [items next request current-item-id]
   (common/layout
