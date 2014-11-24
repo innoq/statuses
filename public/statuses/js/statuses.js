innoq/statuses

commit 03433bd98edcdd96fe7afdc71a133762156422ea

Reply form shouldn't be redrawn for same post
If a reply form is visible and the user clicks the reply button on the same post again, the action should simply be ignored. Otherwise the form is removed, retrieved and displayed again which not only creates visual hassle but also (small) unnecessary traffic on the data plan to retrieve the reply form via HTTP call.

public/statuses/js/statuses.js
@@ -16,11 +16,19 @@
 
 // quick reply
     $(".updates").on("click", ".btn-reply", function (ev) {
-        var oldReplyForm = $("div.new-reply")
+        var post = $(this).closest(".post");
+        var formCount = $("div.new-reply", post);
+        var oldReplyForm = $("div.new-reply");
+        // don't do anything if the form is already present in this post
+        if (formCount[0] && oldReplyForm[0] === formCount[0]) {
+            return;
+        }
+
+        // remove the old form
         oldReplyForm.toggle(200, function () {
             oldReplyForm.remove()
         });
-        var post = $(this).closest(".post");
+
         var postURI = $("a.permalink", post).attr("href");
         $('<div />').addClass("new-reply").
             load(postURI + " form.reply-form", // XXX: introduces duplicate IDs

commit c5441368193342490053009b9c431e294e565b91

fixed bug introduced with 03433bd98: replyform is not focussed
The reply form was not focussed anymore if a user clicked on the reply button of the same post again.

public/statuses/js/statuses.js
@@ -19,8 +19,10 @@
         var post = $(this).closest(".post");
         var formCount = $("div.new-reply", post);
         var oldReplyForm = $("div.new-reply");
+
         // don't do anything if the form is already present in this post
         if (formCount[0] && oldReplyForm[0] === formCount[0]) {
+            $("input", formCount[0]).focus();
             return;
         }
 