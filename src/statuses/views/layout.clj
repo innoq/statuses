02363442482baf6402e418e54e491d91b24ccd08
Michael Vitz <michael.vitz@innoq.com>
2014-12-11 12:23:26 +0100

    Add charset definition to HTML pages
    Specifying the charset prevents the browser from guessing the charset to
    use.
    
    Relates to #130.

--- src/statuses/views/layout.clj
+++ src/statuses/views/layout.clj
@@ -27,6 +27,7 @@
   ([title username content footer]
    (html5
      [:head
+      [:meta {:charset "utf-8"}]
       [:meta {:name "viewport"
               :content "width=device-width, initial-scale=1.0, maximum-scale=1, user-scalable=no"}]
       [:title (str title " - innoQ Statuses")]
