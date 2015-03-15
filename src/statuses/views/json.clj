9e2355cf8e24f0fdb74ee3a33aca938ddeab240b
Simon Koelsch <simon.koelsch@innoq.com>
2015-03-15 21:35:04 +0100

    added link to self in json format

--- src/statuses/views/json.clj
+++ src/statuses/views/json.clj
@@ -1,5 +1,5 @@
 (ns statuses.views.json
-  (:require [statuses.routes :refer [avatar-path profile-path]]))
+  (:require [statuses.routes :refer [avatar-path profile-path update-path]]))
 
 (defn add-avatar [items]
   (map #(assoc % :avatar (avatar-path (:author %))) items))
@@ -7,7 +7,11 @@
 (defn add-profile [items]
   (map #(assoc % :profile (profile-path (:author %))) items))
 
+(defn add-self [items]
+  (map #(assoc % :self (update-path (:id %))) items))
+
 (defn decorate [items]
   (->> items
     (add-avatar)
-    (add-profile)))
+    (add-profile)
+    (add-self)))
