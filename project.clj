af131d20f7eb9f64c4e7ea0244c7dc212c078cbe
Michael Vitz <michael.vitz@innoq.com>
2015-10-14 18:30:40 +0200

    Update compojure from 1.3.3 to 1.4.0

--- project.clj
+++ project.clj
@@ -7,7 +7,7 @@
             :comments "A business-friendly OSS license"}
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [ring "1.4.0"]
-                 [compojure "1.3.3"]
+                 [compojure "1.4.0"]
                  [clj-time "0.9.0"]
                  [org.clojure/data.json "0.2.6"]
                  [org.clojure/tools.cli "0.3.3"]]
