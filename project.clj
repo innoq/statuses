01ead649782c848abe4d0b51e7ed880fb0575905
Michael Vitz <michael.vitz@innoq.com>
2016-03-08 15:51:33 +0100

    Update clojure to 1.8.0

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
             :url "http://www.apache.org/licenses/LICENSE-2.0"
             :distribution :repo
             :comments "A business-friendly OSS license"}
-  :dependencies [[org.clojure/clojure "1.7.0"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
                  [ring "1.4.0"]
                  [compojure "1.4.0"]
                  [clj-time "0.11.0"]
